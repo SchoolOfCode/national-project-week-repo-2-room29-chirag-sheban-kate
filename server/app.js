6d968104802b895c2f66087623537f851327aff7
Sheb95 <80277508+Sheb95@users.noreply.github.com>
2022-01-19 12:31:12 +0000

    server getting everything is running

--- server/app.js
+++ server/app.js
@@ -1,23 +1,26 @@
 import express from "express";
-import cors from "cors";
-import usersRouter from "./routes/users.js";
+// import usersRouter from "./routes/users.js";
+import resources from "./db/resource-data.js";
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+// app.use(express.json());
 
-app.use("/users", usersRouter);
+// app.use("/users", usersRouter);
 
-app.use(function (req, res, next) {
-  res
-    .status(404)
-    .json({ message: "We couldn't find what you were looking for 😞" });
+// app.use(function (req, res, next) {
+//    res.status(404).json({
+//       message: "We couldn't find what you were looking for 😞",
+//    });
+// });
+
+app.get("/", function (req, res, next) {
+   res.json({ message: "here are all the resources", payload: resources });
 });
 
 app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).json(err);
+   console.error(err.stack);
+   res.status(500).json(err);
 });
 
 export default app;
